e9eb59d4b997b956c5665f74f0befd46cae45959
Abdulfatai Olarewaju <olarewaju Abdulfatai>
2026-02-23 15:21:13 +0100

    modified check-auth component

--- Code/src/components/common/check-auth.jsx
+++ Code/src/components/common/check-auth.jsx
@@ -6,6 +6,7 @@ function CheckAuth({ isAuthenticated, user, children, isLoading }) {
   if(isLoading) return (
     <div className="flex items-center justify-center w-screen h-screen bg-black">
       <div className="flex flex-col items-center gap-4">
+        <h1 className="text-yellow-600 text-4xl md:text-5xl">Elegant Store</h1>
         <div className="relative">
           <div className="w-16 h-16 border-4 border-yellow-600 border-t-transparent rounded-full animate-spin"></div>
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
